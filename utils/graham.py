a6583ae5655accba198b130b7942c67ebd31b6db
Dulniko <jakub.dulnik03@gmail.com>
2024-04-22 15:51:36 +0200

    visualization

--- utils/graham.py
+++ utils/graham.py
@@ -1,5 +1,7 @@
-from dataclasses import dataclass
 import math
+from dataclasses import dataclass
+from matplotlib import pyplot as plt
+
 
 @dataclass
 class Point:
@@ -33,3 +35,30 @@ def graham_scan(points):
             hull.pop()
         hull.append(point)
     return hull
+
+def visualize_hull(points, hull):
+    plt.figure()
+    x = [p.x for p in points]
+    y = [p.y for p in points]
+    plt.plot(x, y, 'o', label='Points')
+    hull_points = hull + [hull[0]]
+    hx = [p.x for p in hull_points]
+    hy = [p.y for p in hull_points]
+    plt.plot(hx, hy, 'r-', label='Convex Hull')
+
+    plt.xlabel('X coordinate')
+    plt.ylabel('Y coordinate')
+    plt.title('Convex Hull Visualization')
+    plt.legend()
+    plt.grid(True)
+    plt.savefig('convex_hull.png')
+
+# TODO: delete below code
+if __name__ == '__main__':
+    points = [
+        Point(0, 3), Point(2, 2), Point(1.5, 1), Point(2, 1),
+        Point(3, 0), Point(0, 0), Point(3, -3)
+    ]
+    hull = graham_scan(points)
+    print(hull)
+    visualize_hull(points, hull)
