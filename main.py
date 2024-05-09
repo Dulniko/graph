cbe8d8f2130816e00124138a54218dba1fd2ed19
Dulniko <jakub.dulnik03@gmail.com>
2024-05-09 20:09:42 +0200

    CreateRead points

--- main.py
+++ main.py
@@ -1,16 +1,29 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from typing import List, Dict
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
 
+points: List[Dict[str, int]] = []
+
 
 @app.get("/", response_class=HTMLResponse)
-async def home(request: Request):
+async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 
-@app.get("/get_data")
-async def get_data():
-    return "Mordo, król jest jeden, król Sentino, tylko król Sentino"
+@app.get("/points", response_class=HTMLResponse)
+async def get_points_list(request: Request):
+    return templates.TemplateResponse(
+        "points_list.html", {"request": request, "points": points}
+    )
+
+
+@app.post("/points", response_class=HTMLResponse)
+async def add_point(request: Request, x: int = Form(...), y: int = Form(...)):
+    points.append({"x": x, "y": y})
+    return templates.TemplateResponse(
+        "points_list.html", {"request": request, "points": points}
+    )
