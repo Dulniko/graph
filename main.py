6a81a6f083dcd0f5550e1e6eb18841da707ea282
Dulniko <jakub.dulnik03@gmail.com>
2024-05-10 14:36:20 +0200

    Delete points

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from typing import List, Dict
+from uuid import uuid4
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
@@ -11,7 +12,7 @@ points: List[Dict[str, int]] = []
 
 @app.get("/", response_class=HTMLResponse)
 async def read_root(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+    return templates.TemplateResponse("points.html", {"request": request})
 
 
 @app.get("/points", response_class=HTMLResponse)
@@ -23,7 +24,25 @@ async def get_points_list(request: Request):
 
 @app.post("/points", response_class=HTMLResponse)
 async def add_point(request: Request, x: int = Form(...), y: int = Form(...)):
-    points.append({"x": x, "y": y})
+    points.append({"uuid": str(uuid4()), "x": x, "y": y})
+    return templates.TemplateResponse(
+        "points_list.html", {"request": request, "points": points}
+    )
+
+
+@app.delete("/points/{uuid}", response_class=HTMLResponse)
+async def delete_point(request: Request, uuid: str):
+    for point in points:
+        if point["uuid"] == uuid:
+            points.remove(point)
+    return templates.TemplateResponse(
+        "points_list.html", {"request": request, "points": points}
+    )
+
+
+@app.delete("/points", response_class=HTMLResponse)
+async def clear_points(request: Request):
+    points.clear()
     return templates.TemplateResponse(
         "points_list.html", {"request": request, "points": points}
     )
