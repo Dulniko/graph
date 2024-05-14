980e1badbd8d33a19748798d0118ce03077eda7e
Kamil Szpechciński <kamilsz2002@mat.umk.pl>
2024-05-14 21:18:33 +0200

    fix test_graham.py

--- tests/test_graham.py
+++ tests/test_graham.py
@@ -1,18 +1,18 @@
 import pytest
-from utils.graham import graham_scan, Point
+from utils.graham import Graham, Point
 
 
 @pytest.mark.parametrize(
     "points, expected",
     [
         (
-            [(0, 3), (2, 2), (1, 1), (2, 1), (3, 1), (0, 1), (3, 3)],
-            [(3, 1), (2, 2), (1, 1), (3, 3), (0, 3), (0, 1)],
+            [Point(0, 3), Point(1, 1), Point(2, 2), Point(4, 4), Point(0, 0), Point(1, 2), Point(3, 1), Point(3, 3)],
+            [Point(0, 0), Point(3, 1), Point(4, 4), Point(0, 3)],
         ),
         # TODO: Add more test cases
     ],
 )
 def test_graham_scan(points, expected):
-    points = [Point(x, y) for x, y in points]
-    hull = graham_scan(points)
-    assert hull == [Point(x, y) for x, y in expected]
+    graham = Graham(points)
+    hull = graham.graham_scan()
+    assert hull == expected
