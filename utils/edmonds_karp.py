4175095c79fd0459102da2e9f8bfea1df624977c
Kamil Szpechciński <kamilsz2002@mat.umk.pl>
2024-05-14 17:17:36 +0200

    Separated flows.py into two ford_fulkerson.py and edmods_karp.py. Improved those 2 files by adding docstrings and erasing examples of usage.

---
+++ utils/edmonds_karp.py
@@ -0,0 +1,59 @@
+from collections import deque
+
+
+class EdmondsKarp:
+    def __init__(self, graph):
+        """Initializes the EdmondsKarp instance with a graph.
+
+        As parameter it takes graph (list of list of int).
+        """
+        self.graph = graph
+        self.ROW = len(graph)
+
+    def bfs(self, s, t, parent):
+        """Performs a Breadth-First Search (BFS) to find an augmenting path.
+
+        Returns True if there is an augmenting path from source to sink, False otherwise.
+        """
+        visited = [False] * self.ROW
+        queue = deque()
+        queue.append(s)
+        visited[s] = True
+
+        while queue:
+            u = queue.popleft()
+
+            for ind, val in enumerate(self.graph[u]):
+                if not visited[ind] and val > 0:
+                    queue.append(ind)
+                    visited[ind] = True
+                    parent[ind] = u
+
+        return visited[t]
+
+    def edmonds_karp(self, source, sink):
+        """Computes the maximum flow from source to sink using the Edmonds-Karp algorithm.
+
+        Returns the maximum flow from source to sink.
+        """
+        parent = [-1] * self.ROW
+        max_flow = 0
+
+        while self.bfs(source, sink, parent):
+            path_flow = float("Inf")
+            s = sink
+
+            while s != source:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+
+            max_flow += path_flow
+
+            v = sink
+            while v != source:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
---
+++ utils/ford_fulkerson.py
@@ -0,0 +1,58 @@
+from collections import deque
+
+
+class FordFulkerson:
+    def __init__(self, graph):
+        """Initializes the FordFulkerson instance with a graph.
+
+        As parameter it takes graph (list of list of int).
+        """
+        self.graph = graph
+        self.ROW = len(graph)
+
+    def dfs(self, s, t, parent):
+        """Performs a Depth-First Search (DFS) to find an augmenting path.
+
+        Returns True if there is an augmenting path from source to sink, False otherwise.
+        """
+        visited = [False] * self.ROW
+        stack = [s]
+
+        while stack:
+            u = stack.pop()
+
+            if not visited[u]:
+                visited[u] = True
+                for ind, val in enumerate(self.graph[u]):
+                    if not visited[ind] and val > 0:
+                        stack.append(ind)
+                        parent[ind] = u
+
+        return visited[t]
+
+    def ford_fulkerson(self, source, sink):
+        """Computes the maximum flow from source to sink using the Ford-Fulkerson algorithm.
+
+        Returns the maximum flow from source to sink.
+        """
+        parent = [-1] * self.ROW
+        max_flow = 0
+
+        while self.dfs(source, sink, parent):
+            path_flow = float("Inf")
+            s = sink
+
+            while s != source:
+                path_flow = min(path_flow, self.graph[parent[s]][s])
+                s = parent[s]
+
+            max_flow += path_flow
+
+            v = sink
+            while v != source:
+                u = parent[v]
+                self.graph[u][v] -= path_flow
+                self.graph[v][u] += path_flow
+                v = parent[v]
+
+        return max_flow
