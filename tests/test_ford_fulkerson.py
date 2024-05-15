7bd917304a484e34b70d72071d3c8fa9e0b29497
mlodyg2137 <161320444+mlodyg2137@users.noreply.github.com>
2024-05-15 16:38:40 +0200

    Separated flows.py into two ford_fulkerson.py and edmods_karp.py. (#5)
    * Separated flows.py into two ford_fulkerson.py and edmods_karp.py. Improved those 2 files by adding docstrings and erasing examples of usage.
    
    * added tests to flow algorithms
    
    * flows endpoints
    
    ---------
    
    Co-authored-by: Kamil Szpechciński <kamilsz2002@mat.umk.pl>
    Co-authored-by: Dulniko <jakub.dulnik03@gmail.com>

---
+++ tests/test_ford_fulkerson.py
@@ -0,0 +1,27 @@
+import pytest
+from utils.ford_fulkerson import FordFulkerson
+
+
+@pytest.mark.parametrize(
+    "graph, source, sink, expected",
+    [
+        (
+            [
+                [0, 16, 13, 0, 0, 0],
+                [0, 0, 10, 12, 0, 0],
+                [0, 4, 0, 0, 14, 0],
+                [0, 0, 9, 0, 0, 20],
+                [0, 0, 0, 7, 0, 4],
+                [0, 0, 0, 0, 0, 0],
+            ],
+            0,
+            5,
+            23,
+        ),
+        # TODO: Add more test cases
+    ],
+)
+def test_edmonds_karp(graph, source, sink, expected):
+    ek = FordFulkerson(graph)
+    flow = ek.ford_fulkerson(source, sink)
+    assert flow == expected
